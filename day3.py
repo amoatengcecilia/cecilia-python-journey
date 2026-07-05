207d43e7e204badc461e12d543cd9c2609490e4e
amoatengcecilia <amoatengcecilia1@gmail.com>
2026-07-05 20:34:11 +0000

    Complete Day 3: Loops and iteration practice

---
+++ day3.py
@@ -0,0 +1,25 @@
+# 1. Basic loop
+for i in range(5):
+    print("I am learning Python")
+
+print()
+
+# 2. Numbers
+for i in range(1, 11):
+    print(i)
+print()
+
+# 3. User input loop
+name = input("Enter your name: ")
+for i in range(5):
+    print(name)
+
+print()
+
+# 4. Multiplication table
+a = int(input("Enter number: "))
+for i in range(1, 11):
+   print(f"{a} x {i} = {a * i}")
+
+
+
